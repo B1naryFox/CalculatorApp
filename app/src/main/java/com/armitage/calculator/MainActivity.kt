c8ab0edb9a5bd9c232bb16b4a599411528966713
Elinor Armitage <armitage.elinor@outlook.de>
2025-01-16 14:30:27 +0100

    Project Setup
    - create view layout in activity_main.xml
    - create basic functions in MainActivity.kt

--- app/src/main/java/com/armitage/calculator/MainActivity.kt
+++ app/src/main/java/com/armitage/calculator/MainActivity.kt
@@ -1,20 +1,47 @@
 package com.armitage.calculator
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    val calculationStack = mutableListOf<String>()
+    lateinit var textViewCalculation: TextView
+    var stringBuilder = StringBuilder()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        textViewCalculation = findViewById(R.id.textViewCalculation)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+    }
+
+    /**
+     * When clicking a button with a number or the decimal point the character will be appended to
+     * the String in the TextView
+     * @param view
+     */
+    fun btnClickNum(view: View) {
+        val btn = findViewById<Button>(view.id)
+        stringBuilder.append(textViewCalculation.text)
+        stringBuilder.append(btn.text)
+        textViewCalculation.text = stringBuilder.toString()
+        stringBuilder.clear()
     }
 }
+
+
+
